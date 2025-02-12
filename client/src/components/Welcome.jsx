4eef06d24b5984bddbcbe1293db9d1a01376cf44
Mawia <mawia@localhost.localdomain>
2025-02-12 21:52:25 +0300

    worked on the welcome page, added the inputs and the card

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -2,7 +2,22 @@ import { AiFillPlayCircle } from "react-icons/ai";
 import { SiEthereum } from "react-icons/si";
 import{BsInfoCircle } from "react-icons/bs";
 import{ Loader } from './';
+
 const commonStyles='min-h-[70px] sm:px-0 sm:min-w-[120px] flex justify-center items-center border-[0.5px] border-gray-400 text-white'
+
+const Input = ({ placeholder, name, type, value, handleChange}) => (
+    <input 
+
+    placeholder={placeholder}
+    type={type}
+    step="0.0001"
+    value={value}
+    onChange={(e) => handleChange(e, name)}
+    className="my-2 w-full rounded-sm p-2 outline-none bg-transparent text-white border-none text-sm white-glassmorphism"
+    />
+
+);
+
 const Welcome =() => {
     const connectWallet= () => {}
     return (
@@ -53,7 +68,37 @@ const Welcome =() => {
 
                 </div>
                 <div className="flex flex-col flex-1 items-center justify-start w-full md:mt-0 mt-10">
-                    <div className="">
+                    <div className="p-3 justify-end items-start flex-col rounded-xl h-40 sm:w-72 w-full my-5 eth-card white-glassmorphism">
+                        <div className="flex justify-between flex-col w-full h-full">
+                            <div className="flex justify-between items-start">
+                                <div className="w-10 h-10 rounded-full border-2 border-white flex justify-center items-center">
+                                    <SiEthereum fontSize={21} color="#fff" />
+
+                                </div>
+                                <BsInfoCircle fontSize={17} color="#fff" />
+
+                            </div>
+                            <div>
+                                <p className="text-white font-light text-sm"> 
+                                    0xasdfhs....assdj
+
+                                </p>
+                                <p className="text-white font-light text-lg mt-1"> 
+                                    Ethereum
+
+                                </p>
+                            </div>
+
+                        </div>
+
+                    </div>
+
+                    <div className="p-5 sm:w-96 w-full flex flex-col justify-start items-center blue-glassmorphism">
+                        <Input placeholder="Address To" name="addressTo" type="text" handleChange={ () => {}}  />
+                        <Input placeholder="Address To" name="addressTo" type="text" handleChange={ () => {}}  />
+                        <Input placeholder="Address To" name="addressTo" type="text" handleChange={ () => {}}  />
+                        <Input placeholder="Address To" name="addressTo" type="text" handleChange={ () => {}}  />
+
 
                     </div>
 
